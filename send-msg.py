ba3b8d2aed20bb7cfa3791d8401a15287cd5282c
0101011 <andrew.stepin@gmail.com>
2019-09-11 12:25:18 +0300

    Create send-msg.py

---
+++ send-msg.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+# Type your cell phone number with WhatsApp installed:
+your_num = '+123456789'
+
+# Cell phone number of the reciver of your messages:
+to = '+987654321'
+
+# Reading client credentials from TWILIO_ACCOUNT_SID and AUTH_TOKEN:
+client = Client()
+
+# Defining sender and receiver of messages:
+from_num = f'whatsapp:{your_num}'
+to_num = f'whatsapp:{to}'
